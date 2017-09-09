104b5797520cf3d8e967ac77a096bda63f028ac8
Sam Bengtson <sam.bengtson@gmail.com>
2017-09-09 15:00:41 -0500

    initial commit

---
+++ CryptoTrayForms/Api.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using System.Runtime.Serialization;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace CryptoTrayForms
+{
+    public class Api
+    {
+        RestClient client;
+
+        public Api()
+        {
+            client = new RestClient("https://api.cryptonator.com/api/ticker");
+        }
+
+        public List<CryptResponse> GetMajorCryptoPrices()
+        {
+            var btc = GetBTC();
+            var ltc = GetLTC();
+            var eth = GetETH();
+
+            var prices = new List<CryptResponse>();
+            prices.Add(btc.Result);
+            prices.Add(ltc.Result);
+            prices.Add(eth.Result);
+
+            return prices;
+        }
+
+        private async Task<CryptResponse> GetBTC()
+        {
+            var req = new RestRequest("/btc-usd", Method.GET);
+            
+            var response = await client.ExecuteTaskAsync(req);
+            if (response.ErrorException != null)
+            {
+                throw response.ErrorException;
+            }
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception("Invalid status code");
+            }
+
+            return JsonConvert.DeserializeObject<CryptResponse>(response.Content);
+        }
+
+        private async Task<CryptResponse> GetLTC()
+        {
+            var req = new RestRequest("/ltc-usd", Method.GET);
+
+            var response = await client.ExecuteTaskAsync(req);
+            if (response.ErrorException != null)
+            {
+                throw response.ErrorException;
+            }
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception("Invalid status code");
+            }
+
+            return JsonConvert.DeserializeObject<CryptResponse>(response.Content);
+        }
+
+        private async Task<CryptResponse> GetETH()
+        {
+            var req = new RestRequest("/eth-usd", Method.GET);
+
+            var response = await client.ExecuteTaskAsync(req);
+            if (response.ErrorException != null)
+            {
+                throw response.ErrorException;
+            }
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception("Invalid status code");
+            }
+
+            return JsonConvert.DeserializeObject<CryptResponse>(response.Content);
+        }
+    }
+
+    public class CryptResponse
+    {
+        [JsonProperty(PropertyName = "ticker")]
+        public Ticker Response { get; set; }
+
+        [JsonProperty(PropertyName = "success")]
+        public bool IsSuccess { get; set; }
+    }
+    
+    public class Ticker
+    {
+        [JsonProperty(PropertyName = "base")]
+        public string Base { get; set; }
+
+        [JsonProperty(PropertyName = "target")]
+        public string Target { get; set; }
+
+        [JsonProperty(PropertyName = "price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty(PropertyName = "volume")]
+        public decimal Volume { get; set; }
+
+        [JsonProperty(PropertyName = "change")]
+        public decimal Change { get; set; }
+    }
+}
---
+++ CryptoTrayForms/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Text;
+
+namespace CryptoTrayForms
+{
+    static class Program
+    {
+        static Api client;
+
+        [STAThread]
+        static void Main()
+        {
+            client = new CryptoTrayForms.Api();
+
+            Thread t = new Thread((f) =>
+            {
+                System.Timers.Timer apiTimer = new System.Timers.Timer();
+                apiTimer.Elapsed += T_Elapsed;
+
+#if DEBUG
+                apiTimer.Interval = new TimeSpan(0, 1, 0).TotalMilliseconds;
+#endif
+#if !DEBUG
+                apiTimer.Interval = new TimeSpan(0, 10, 0).TotalMilliseconds;
+#endif
+
+                apiTimer.Start();
+
+                //Keep this thread alive
+                while(true)
+                {
+                    Thread.Sleep(10000);
+                }                                   
+            });
+            t.Start();            
+        }
+
+        private static void T_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                var prices = client.GetMajorCryptoPrices();
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var price in prices)
+                {
+                    if (price.IsSuccess)
+                    {
+                        sb.AppendLine($"{price.Response.Base} - {decimal.Round(price.Response.Price, 2)} - Change: {decimal.Round(price.Response.Change, 2)}");
+                    }
+                }
+
+                var msg = sb.ToString();
+                if (msg.Length > 0)
+                {
+                    ShowNotification(sb.ToString());
+                }
+            }
+            catch(Exception ex)
+            {
+                ShowNotification($"An error occured while obtaining crypto results: {ex.Message}");
+            }         
+        }
+
+
+        static void ShowNotification(string msg)
+        {
+            var notification = new System.Windows.Forms.NotifyIcon()
+            {
+                Visible = true,
+                Icon = System.Drawing.SystemIcons.Information,
+                BalloonTipTitle = "Current Crypto Rates",
+                BalloonTipText = msg,
+            };
+
+            var path = Environment.CurrentDirectory + "\\coin_VAc_icon.ico";
+            notification.Icon = new System.Drawing.Icon(path);
+
+            notification.ShowBalloonTip(10000);
+            Thread.Sleep(10000);
+            notification.Dispose();
+        }
+    }
+}
